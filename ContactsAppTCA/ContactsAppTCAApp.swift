87649efc4d869a968f40ca90b1e2dd7f8a384cd7
Andrew Stoddart <andrew.stoddart@abc.com>
2025-09-12 07:44:07 -0500

    Initial Commit

---
+++ ContactsAppTCA/ContactsAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppTCAApp.swift
+//  ContactsAppTCA
+//
+//  Created by Andrew Stoddart on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
